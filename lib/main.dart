f6c043538fe91c3d787f1432f20919cb57945af4
Yogi Patel <yogi@yomagic.co.uk>
2024-09-11 19:53:44 +0100

    Lock the app to portait mode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'widgets/expenses.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(home: Expenses()),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(
+        const MaterialApp(home: Expenses()
+        )
+    );
+  });
 }
